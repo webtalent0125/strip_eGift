afbe8beb057dd4d21f64a1fc9f41c4a0867a22ab
webtalent0125 <talenteddev0125@gmail.com>
2023-08-25 06:12:33 +0900

    amplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Amplify } from 'aws-amplify';
+// import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 
 import App from './App';
